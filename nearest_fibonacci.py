c88ce10652259836ab7ec72a2da25c80e96d6230
J B V K SAI GANESH NAKKINA <80577170+saiganeshnakkina@users.noreply.github.com>
2022-10-03 21:28:00 +0530

    n=int(input()) a,b=0,1 while(True):     c=a+b     if(c>n):        h1=b        h2=c        break     a=b     b=c
    x1=abs(h1-n)
    x2=abs(h2-n)
    if(x1==x2):
       print(h1,h2)
    elif(x1>x2):
       print(h2)
    else:
       print(h1)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,25 @@
+n=int(input())
+a,b=0,1
+while(True):
+    c=a+b
+    if(c>n):
+       h1=b
+       h2=c
+       break
+    a=b
+    b=c
+
+
+
+x1=abs(h1-n)
+x2=abs(h2-n)
+if(x1==x2):
+   print(h1,h2)
+elif(x1>x2):
+   print(h2)
+else:
+   print(h1)
+   
+   
+   
+   
